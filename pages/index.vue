501776f75d59683112ef634c63ef63c555deef63
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2019-05-02 08:57:28 +0900

    components > icons > fb.vue を作成し、pages > index.vue に配置

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,33 @@
       </div>
     </header>
     <Nav />
+    <div class="sns">
+      <ul>
+        <li class="sns__list fb">
+          <a
+            href="https://www.facebook.com/masaaki.todoroki.5"
+            target="_blank"
+            class="sns__link"
+          >
+            <IconFb />
+            <span class="bk"></span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import Canvas from '~/components/index/Canvas.vue'
 import Nav from '~/components/common/Nav.vue'
+import IconFb from '~/components/icons/fb.vue'
 
 export default {
   components: {
     Canvas,
-    Nav
+    Nav,
+    IconFb
   }
 }
 </script>
