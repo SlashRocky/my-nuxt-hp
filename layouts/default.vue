e4c895792cc725aa75f7ef412207b4bd8a051882
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2019-05-02 06:49:11 +0900

    default.vueの編集

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,17 +4,74 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
+html,
+body {
+  @include mq() {
+    width: 100%;
+    overflow-x: hidden;
+  }
+}
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
+  font-family: 'Noto Sans JP', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Meiryo, Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 15px;
+  font-weight: 400;
+  color: $text-color;
+  line-height: 1.7;
+  font-feature-settings: 'palt' 1;
+  letter-spacing: 0.01em;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  overflow-x: hidden;
+  -webkit-font-smoothing: antialiased;
+  @include mq() {
+    font-size: 14px;
+  }
+}
+
+a {
+  color: $text-color;
+  text-decoration: none;
+  &:visited {
+    color: $text-color;
+  }
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-weight: 500;
+  line-height: 1.3;
+}
+
+h1 {
+  font-size: 32px;
+}
+
+h2 {
+  font-size: 26px;
+}
+
+h3 {
+  font-size: 21px;
+}
+
+h4 {
+  font-size: 18px;
+}
+
+::selection {
+  background: $text-color;
+  color: #ffffff;
+}
+
+.jos,
+._jos {
+  font-family: 'Josefin Sans', sans-serif;
 }
 
 *,
@@ -22,34 +79,5 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
