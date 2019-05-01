f30bf37f23a912b657875c2256e43ac66ca8a1e3
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2019-05-02 06:16:49 +0900

    .eslintrc.jsを編集

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,33 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-    'prettier/vue'
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
   ],
   plugins: [
+    'vue',
     'prettier'
   ],
   // add your custom rules here
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV ===  'production' ? 'error' : 'off',
+    //allow async-await
+    'generator-star-spacing': 'off',
+    //add
+    'semi': [2, 'never'],
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-unused-commponents': 'off',
+    'vue/no-v-html': 'off',
+    'prettier/prettier': ['error', {
+      'singleQuote': true,
+      'semi': false
+    }],
+    'vue/html-self-closing': ['error', {
+      'html': {
+        'normal': 'never',
+        'void': 'always'
+      }
+    }]
   }
 }
