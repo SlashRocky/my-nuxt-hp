6860dad1e2140553eafc1fd16c47a4002406464b
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2019-05-02 09:25:59 +0900

    components > icons > github.vue を作成し、pages > index.vue に配置

--- pages/index.vue
+++ pages/index.vue
@@ -4,32 +4,42 @@
       <div class="background">
         <Canvas />
       </div>
+      <Nav />
+      <div class="sns">
+        <ul>
+          <li class="sns__list fb">
+            <a
+              href="https://www.facebook.com/masaaki.todoroki.5"
+              target="_blank"
+              class="sns__link"
+            >
+              <IconFb />
+              <span class="bk"></span>
+            </a>
+          </li>
+          <li class="sns__list tw">
+            <a
+              href="https://twitter.com/Slash_Rocky"
+              target="_blank"
+              class="sns__link"
+            >
+              <IconTw />
+              <span class="bk"></span>
+            </a>
+          </li>
+          <li class="sns__list github">
+            <a
+              href="https://github.com/SlashRocky"
+              target="_blank"
+              class="sns__link"
+            >
+              <IconGithub />
+              <span class="bk"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </header>
-    <Nav />
-    <div class="sns">
-      <ul>
-        <li class="sns__list fb">
-          <a
-            href="https://www.facebook.com/masaaki.todoroki.5"
-            target="_blank"
-            class="sns__link"
-          >
-            <IconFb />
-            <span class="bk"></span>
-          </a>
-        </li>
-        <li class="sns__list tw">
-          <a
-            href="https://twitter.com/Slash_Rocky"
-            target="_blank"
-            class="sns__link"
-          >
-            <IconTw />
-            <span class="bk"></span>
-          </a>
-        </li>
-      </ul>
-    </div>
   </div>
 </template>
 
@@ -38,13 +48,15 @@ import Canvas from '~/components/index/Canvas.vue'
 import Nav from '~/components/common/Nav.vue'
 import IconFb from '~/components/icons/fb.vue'
 import IconTw from '~/components/icons/tw.vue'
+import IconGithub from '~/components/icons/github.vue'
 
 export default {
   components: {
     Canvas,
     Nav,
     IconFb,
-    IconTw
+    IconTw,
+    IconGithub
   }
 }
 </script>
