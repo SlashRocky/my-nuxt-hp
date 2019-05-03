713c5b8cb4def37863896ec210ac56b6092809db
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2019-05-03 22:51:30 +0900

    components > icons > heart.vue を作成し、Footer.vueに配置

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,9 +1,31 @@
 <template>
-  <div></div>
+  <footer class="footer">
+    <div class="footer__line"></div>
+    <div class="footer__copy">
+      <p class="jos">
+        <span>©</span>
+        <span
+          v-for="(text, key) in 'MasaakiTodoroki'"
+          :key="`name-text-${key}`"
+        >
+          {{ text }}
+        </span>
+        <span class="heart">
+          <IconHeart />
+        </span>
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {}
+import IconHeart from '~/components/icons/heart.vue'
+
+export default {
+  components: {
+    IconHeart
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
