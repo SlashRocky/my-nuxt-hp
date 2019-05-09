bbb88c72fd8f9e1b353f1d02bc2c80a7136dba6f
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2019-05-09 16:47:11 +0900

    components > common > LoadingLogo.vue を作成し、Loading.vue に配置

--- components/common/Loading.vue
+++ components/common/Loading.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <div id="loading">
+    <div id="loadingMask" class="loading__mask"></div>
+    <div id="loadingBack" class="loading__back">
+      <div
+        v-for="n in 40"
+        :key="`loading-element-${n}`"
+        class="loading__element"
+      ></div>
+    </div>
+    <div id="loadingFront" class="loading__front">
+      <div class="loading__logo">
+        <LoadingLogo />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import LoadingLogo from '~/components/common/LoadingLogo.vue'
+
+export default {
+  components: {
+    LoadingLogo
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
