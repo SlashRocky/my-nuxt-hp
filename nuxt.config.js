ff73ccbdcdeb1f273c3fb79af0a7c4537c9c185d
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2019-05-09 16:35:34 +0900

    components > common > Loading.vue を作成し、nuxt.config.js に登録

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: '~/components/common/Loading.vue',
   router: {
     linkActiveClass: 'is-current'
   },
